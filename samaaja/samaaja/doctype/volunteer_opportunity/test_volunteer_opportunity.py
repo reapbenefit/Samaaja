40ca7e8491f0a73a84fe57c18c4d77322ef43896
Ankit Saxena <saxenankit@gmail.com>
2026-08-18 08:39:50 +0000

    added basic volunteer feature

---
+++ samaaja/samaaja/doctype/volunteer_opportunity/test_volunteer_opportunity.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Impactyaan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVolunteerOpportunity(FrappeTestCase):
+	pass
